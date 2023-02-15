363a3221aa6ce160626945c70befa0882e96002f
batoriandras <batori.andras@gmail.com>
2023-02-15 13:08:15 +0100

    Vue login site

---
+++ TamagotchiVue/tamagotchi-vue/src/components/Login.vue
@@ -0,0 +1,41 @@
+<template>
+        <div class="row mb-3">
+            <div class="col">
+                <h2>Login</h2>
+            </div>
+        </div>
+        <div class="row p-4">
+            <div class="col">
+                <form>
+                    <label for="username">Username</label><br>
+                    <input type="text" name="username" id="username" class="form-control">
+
+                    <label for="pwd">Password</label> <br>
+                    <input type="password" name="pwd" id="pwd" class="form-control">
+                    <br>
+                    <input class="btn btn-light w-50" type="submit" value="Go">
+                </form>
+            </div>
+        </div>
+</template>
+<style scoped>
+.row {
+    background-color: #D89E0A;
+    border-radius: 40px;
+    color: black;
+    width: 70%;
+    text-align: center;
+    margin: auto;
+}
+
+input {
+    width: 70%;
+    margin: auto;
+    border-radius: 40px;
+}
+
+label {
+    padding: 10px;
+    font-size: larger;
+}
+</style>
---
+++ TamagotchiVue/tamagotchi-vue/src/views/LoginView.vue
@@ -0,0 +1,29 @@
+<script>
+import Login from '../components/Login.vue'
+export default {
+    components: { Login }
+}
+</script>
+
+<template>
+    <div class="container-fluid">
+        <Login />
+    </div>
+</template>
+<style scoped>
+.container-fluid {
+    width: 800px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    margin: auto;
+    margin-top: 10%;
+
+}
+</style>
+<style>
+body {
+    backdrop-filter: blur(3px);
+    background-image: url(/login-bg.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>
