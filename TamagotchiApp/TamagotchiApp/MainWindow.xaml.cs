bb7bf8fa7fe00e91665b141c68be9a347d2fc4ec
Krisztian <hollokg@gmail.com>
2023-02-21 12:36:52 +0100

    wpf new animal and all account button added without backend

--- TamagotchiApp/TamagotchiApp/MainWindow.xaml.cs
+++ TamagotchiApp/TamagotchiApp/MainWindow.xaml.cs
@@ -25,44 +25,17 @@ namespace TamagotchiApp
         public MainWindow()
         {
             InitializeComponent();
-            Timer();
+            
         }
-        public void Timer()
-        {
-            DispatcherTimer time = new System.Windows.Threading.DispatcherTimer();
-            time.Tick += new EventHandler(DecreaseStas);
-            time.Interval = new TimeSpan(0, 0, 10);
-            time.Start();
-        }
-        private void DecreaseStas(object sender, EventArgs e)
-        {
-            bool asd = false;
-            hunger.Value -= 10;
-            thirst.Value -= 15;
-            if (hunger.Value <= 75 || thirst.Value <= 75 && asd != true)
-            {
-                mood.Value -= 30;
-                asd = true;
-            }
-        }
-        private void hunger_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+
+        private void NewAnimal_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void AllAccount_Click(object sender, RoutedEventArgs e)
         {
-            hunger.Value += 30;
-        }
 
-        private void drink_Click(object sender, RoutedEventArgs e)
-        {
-            thirst.Value += 30;
-        }
-
-        private void play_Click(object sender, RoutedEventArgs e)
-        {
-            mood.Value += 50;
         }
     }
 }
