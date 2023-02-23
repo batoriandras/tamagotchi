8fb9b668aaa085b6d051130fe12d46704e8384c1
batoriandras <batori.andras@gmail.com>
2023-02-23 18:21:59 +0100

    selenium, vue

--- SeleniumTest/SeleniumTest/Program.cs
+++ SeleniumTest/SeleniumTest/Program.cs
@@ -37,11 +37,11 @@ namespace SeleniumTest
             #endregion
 
             #region eat
-            int hunger = Convert.ToInt32(driver.FindElement(By.CssSelector("li:nth-child(3)>p:nth-child(2)")).Text);
+            int hunger = Convert.ToInt32(driver.FindElement(By.CssSelector("#statul>li:nth-child(3)>p:nth-child(2)")).Text);
             var eatBTN = driver.FindElement(By.Id("eat"));
             eatBTN.Click();
             Console.ReadKey();
-            int hunger2 = Convert.ToInt32(driver.FindElement(By.CssSelector("li:nth-child(3)>p:nth-child(2)")).Text);
+            int hunger2 = Convert.ToInt32(driver.FindElement(By.CssSelector("#statul>li:nth-child(3)>p:nth-child(2)")).Text);
             if (hunger == hunger2 - 20)
             {
                 Console.WriteLine("Hunger level went up by 20");
@@ -53,11 +53,11 @@ namespace SeleniumTest
             #endregion
 
             #region drink
-            int thirst = Convert.ToInt32(driver.FindElement(By.CssSelector("li:nth-child(4)>p:nth-child(2)")).Text);
+            int thirst = Convert.ToInt32(driver.FindElement(By.CssSelector("#statul>li:nth-child(4)>p:nth-child(2)")).Text);
             var drinkBTN = driver.FindElement(By.Id("drink"));
             drinkBTN.Click();
             Console.ReadKey();
-            int thirst2 = Convert.ToInt32(driver.FindElement(By.CssSelector("li:nth-child(3)>p:nth-child(2)")).Text);
+            int thirst2 = Convert.ToInt32(driver.FindElement(By.CssSelector("#statul>li:nth-child(4)>p:nth-child(2)")).Text);
             if (thirst == thirst2 - 20)
             {
                 Console.WriteLine("Thirst level went up by 20");
@@ -69,11 +69,11 @@ namespace SeleniumTest
             #endregion
 
             #region hunt
-            int mood = Convert.ToInt32(driver.FindElement(By.CssSelector("li:nth-child(2)>p:nth-child(2)")).Text);
+            int mood = Convert.ToInt32(driver.FindElement(By.CssSelector("#statul>li:nth-child(2)>p:nth-child(2)")).Text);
             var huntBTN = driver.FindElement(By.Id("hunt"));
             huntBTN.Click();
             Console.ReadKey();
-            int mood2 = Convert.ToInt32(driver.FindElement(By.CssSelector("li:nth-child(3)>p:nth-child(2)")).Text);
+            int mood2 = Convert.ToInt32(driver.FindElement(By.CssSelector("#statul>li:nth-child(2)>p:nth-child(2)")).Text);
             if (mood == mood2 - 70)
             {
                 Console.WriteLine("Mood level went up by 70");
@@ -88,7 +88,7 @@ namespace SeleniumTest
             var petBTN = driver.FindElement(By.Id("pet"));
             petBTN.Click();
             Console.ReadKey();
-            mood = Convert.ToInt32(driver.FindElement(By.CssSelector("li:nth-child(3)>p:nth-child(2)")).Text);
+            mood = Convert.ToInt32(driver.FindElement(By.CssSelector("#statul>li:nth-child(2)>p:nth-child(2)")).Text);
             if (mood2 == mood - 20)
             {
                 Console.WriteLine("Mood level went up by 20");
@@ -96,6 +96,19 @@ namespace SeleniumTest
             else
             {
                 Console.WriteLine("asd");
+            }
+            #endregion
+
+            #region account page
+            var accBTN = driver.FindElement(By.XPath("//a[@href='/account']"));
+            accBTN.Click();
+            if (driver.Url== "http://localhost:5174/account")
+            {
+                Console.WriteLine("Account page loaded successfully");
+            }
+            else
+            {
+                Console.WriteLine("Account page failed to load successfully");
             }
             #endregion
 
@@ -112,6 +125,20 @@ namespace SeleniumTest
                 Console.WriteLine("Log out unsuccessful");
             }
             #endregion
+
+            #region sign up
+            var signupBTN = driver.FindElement(By.XPath("//a[@href='/signup']"));
+            signupBTN.Click();
+            if (driver.Url == "http://localhost:5174/signup")
+            {
+                Console.WriteLine("Sign up page loaded successfully");
+            }
+            else
+            {
+                Console.WriteLine("Sign up page failed to load successfully");
+            }
+            #endregion
+
             Console.ReadKey();
             driver.Close();
         }
