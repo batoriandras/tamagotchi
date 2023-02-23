8c04ae73b21ecc93b8ba5632c02812ec04f547a8
Nagy Ádám Attila <adamattila02@gmail.com>
2023-02-23 12:29:47 +0100

    Pet actions implemented, eat, drink, hun, pet actions are working with the current pet

--- TamagotchiVue/tamagotchi-vue/src/components/PetAction.vue
+++ TamagotchiVue/tamagotchi-vue/src/components/PetAction.vue
@@ -1,43 +1,77 @@
 <template>
     <div class="row p-3">
-        <div class="col"><button class="btn" @click="incHunger()">Eat</button></div>
-        <div class="col"><button class="btn" @click="incThirst()">Drink</button></div>
-        <div class="col"><button class="btn" @click="incMood()">Hunt</button></div>
-        <div class="col"><button class="btn" @click="incMood()">Pet</button></div>
+        <div class="col"><button class="btn" @click="editHunger()">Eat</button></div>
+        <div class="col"><button class="btn" @click="editThirst()">Drink</button></div>
+        <div class="col"><button class="btn" @click="huntMood()">Hunt</button></div>
+        <div class="col"><button class="btn" @click="editMood()">Pet</button></div>
     </div>
 </template>
-<script setup>
-import { http } from '../helper/http';
-import {useRouter} from "vue-router";
-const router = useRouter();
+<script>
+import { http } from '../helper/http'
+import moment from 'moment'
+import { VueElement } from 'vue';
+export default{
+    data(){
+        return{
+            pet: {},
+            obj: {},
+            date: new Date()
+        }
+    },
+    methods:{
+        async petStats(){
+            const response = await http.get('pet/'+localStorage.getItem('userid'));
+            this.pet = response.data.data;
+            
+        },
+        async editHunger(){
+            this.obj = {
+                hunger: this.pet.hunger + 20,
+                thirst: this.pet.thirst,
+                mood: this.pet.mood,
+                fatigue: this.pet.fatigue
+            }
+            const response = await http.put('editpetstat/'+localStorage.getItem('userid'), this.obj);
+            window.location.reload();
+        },
+        async editThirst(){
+            this.obj = {
+                hunger: this.pet.hunger,
+                thirst: this.pet.thirst + 20,
+                mood: this.pet.mood,
+                fatigue: this.pet.fatigue
+            }
+            const response = await http.put('editpetstat/'+localStorage.getItem('userid'), this.obj);
+            window.location.reload();
+        },
+        async editMood(){
+            this.obj = {
+                hunger: this.pet.hunger,
+                thirst: this.pet.thirst,
+                mood: this.pet.mood + 20,
+                fatigue: this.pet.fatigue
+            }
+            const response = await http.put('editpetstat/'+localStorage.getItem('userid'), this.obj);
+            window.location.reload();
+        },
+        async huntMood(){
+            this.obj = {
+                hunger: this.pet.hunger,
+                thirst: this.pet.thirst,
+                mood: this.pet.mood + 60,
+                fatigue: this.pet.fatigue
+            }
+            const response = await http.put('editpetstat/'+localStorage.getItem('userid'), this.obj);
+            window.location.reload();
+        }
+    },
+    mounted(){
+        this.petStats();
+    }
+}
 
-    function incMood(){
-        
-        http.put('editpetstat/' + localStorage.getItem('userid'),
-        {
-            mood: 10,
-            thirst: thirst,
-            hunger: hunger,
-            fatigue: fatigue
-        });
-        router.push({name: 'pet'});
-    }
-    function incThirst(){
-        
-        http.put('editpetstat/' + localStorage.getItem('userid'),
-        {
-            thirst: 10
-        });
-        router.push({name: 'pet'});
-    }
-    function incHunger(){
-        
-        http.put('editpetstat/' + localStorage.getItem('userid'),
-        {
-            hunger: 10
-        });
-        router.push({name: 'pet'});
-    }
+
+
 
 </script>
 <style scoped>
